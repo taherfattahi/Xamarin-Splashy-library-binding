e8ae2bbbf6e8291103f43e5dbe960e365ac75019
taherfattahi <taherfattahi11@gmail.com>
2020-02-06 19:16:25 +0330

    first commit

---
+++ MainActivity.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+using Com.Rbddevs.Splashy;
+
+namespace XamarinSplashyJavaLibraryBinding
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            Splashy splashy = new Splashy(this);
+            splashy
+              .SetLogo(Resource.Drawable.splashy)
+              .SetAnimation(Splashy.Animation.GrowLogoFromCenter, 800)
+              .SetBackgroundResource(Resource.Color.black)
+              .SetTitleColor(Resource.Color.white)
+              .SetProgressColor(Resource.Color.white)
+              .SetTitle(Resource.String.splashy)
+              .SetSubTitle(Resource.String.splash_screen_made_easy)
+              .SetFullScreen(true)
+              .SetClickToHide(true)
+              //.setInfiniteDuration(true)
+              .SetTime(5000)
+              .Show();
+
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
+
---
+++ obj/Debug/android/typemap.mj.x86.s
@@ -0,0 +1,25 @@
+	/* Data Hash: 8AF0C9FD8BF663A9 */
+	.file	"typemap.mj.inc"
+
+	/* Mapping header */
+	.section	.data.mj_typemap,"aw",@progbits
+	.type	mj_typemap_header, @object
+	.p2align	2
+	.global	mj_typemap_header
+mj_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	1693
+	/* entry-length */
+	.long	262
+	/* value-offset */
+	.long	145
+	.size	mj_typemap_header, 16
+
+	/* Mapping data */
+	.type	mj_typemap, @object
+	.global	mj_typemap
+mj_typemap:
+	.size	mj_typemap, 443567
+	.include	"typemap.mj.inc"
